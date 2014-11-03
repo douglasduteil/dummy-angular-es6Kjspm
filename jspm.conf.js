26d08f00a38e0fa29ca5fcce2669986045d0b20b
Douglas Duteil <douglasduteil@gmail.com>
2014-11-03 01:51:48 +0100

    feat: angular update to 1.3.1

--- jspm.conf.js
+++ jspm.conf.js
@@ -7,15 +7,15 @@ System.config({
 
 System.config({
   "map": {
-    "angular": "github:angular/bower-angular@^1.3.0",
-    "angular-mocks": "github:angular/bower-angular-mocks@^1.3.0"
+    "angular": "github:angular/bower-angular@^1.3.1",
+    "angular-mocks": "github:angular/bower-angular-mocks@^1.3.1"
   }
 });
 
 System.config({
   "versions": {
-    "github:angular/bower-angular": "1.3.0",
-    "github:angular/bower-angular-mocks": "1.3.0"
+    "github:angular/bower-angular": "1.3.1",
+    "github:angular/bower-angular-mocks": "1.3.1"
   }
 });
 
